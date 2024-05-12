4a34dd89b6cf7a700e3dea3522a9b840107f19a0
MiniAppleTheApple <Chenxiuhua19780828@gmail.com>
2024-05-12 19:27:25 -0300

    refactor: mark unused arguments as unused

--- src/Cards.tsx
+++ src/Cards.tsx
@@ -8,8 +8,8 @@ const Cards: React.FC<CardsProps> = ({remove, edit, cards, selected}) => {
           <div className="text-xl flex justify-between">
             <span className={`mx-4 my-auto ${selected.type === "card" && selected.action.type === "edit" && selected.action.index === index ? "text-timberwolf-50 font-bold" : ""}`}>{x.text} - {x.answer}</span>
             <div>
-              <button className={primaryButton} onClick={e => edit(index)}>Edit</button>
-              <button className={secondaryButton} onClick={e => remove(index)}>Delete</button>
+              <button className={primaryButton} onClick={_e => edit(index)}>Edit</button>
+              <button className={secondaryButton} onClick={_e => remove(index)}>Delete</button>
             </div>
           </div>
         </li>)}
